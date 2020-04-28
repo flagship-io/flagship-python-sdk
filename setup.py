4172b29df4987f9ac5727a9cc7b9086caae1dbbf
guillaume <guillaume.jacquart@abtasty.com>
2020-04-28 09:04:49 +0200

    Add setup.py (for local import)

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='flagship',
+    version='0.0.1',
+    author="Flagship Team",
+    author_email="support@flagship.io",
+    description="Flagship Python SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/abtasty/flagship-python-sdk",
+    packages=setuptools.find_packages()
+)
