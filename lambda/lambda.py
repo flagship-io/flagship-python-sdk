331e799cb231863d2d85e6fc66c951c460c9893d
= <raphael@abtasty.com>
2024-01-04 11:46:55 +0100

    clean lambda

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -3,24 +3,19 @@ from flagship.config import Bucketing
 from flagship.hits import Screen
 from flagship.tracking_manager import TrackingManagerConfig
 
-#
-# #global
-#
-Flagship.start('__env_id__', '__apikey__', Bucketing(timeout=3000,
-                                                     log_level=LogLevel.ALL,
-                                                     tracking_manager_config=TrackingManagerConfig(
-                                                         time_interval=5000,
-                                                         max_pool_size=5)))  ## Demo
+Flagship.start('__env_id__', '__api_key__', Bucketing(timeout=3000,
+                                                      log_level=LogLevel.ALL,
+                                                      tracking_manager_config=TrackingManagerConfig(
+                                                          time_interval=5000,
+                                                          max_pool_size=5)))
+
 
 def lambda_handler(event, context):
-
     visitor = Flagship.new_visitor('visitor-AAA', context={'isVIPUser': True})
     visitor.fetch_flags()
     value = visitor.get_flag("featureEnabled", False).value()
-    
+
     visitor.send_hit(Screen("screen 1"))
 
     Flagship.stop()
     return value
-
-
